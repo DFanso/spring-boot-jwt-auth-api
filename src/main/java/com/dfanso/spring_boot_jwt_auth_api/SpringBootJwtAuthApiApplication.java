6a712a877f4f38c061e037648760d0904e4cd4ab
Leo Gavin <leogavin123@outlook.com>
2024-05-24 18:43:16 +0530

    init

---
+++ src/main/java/com/dfanso/spring_boot_jwt_auth_api/SpringBootJwtAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.dfanso.spring_boot_jwt_auth_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtAuthApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dfanso/spring_boot_jwt_auth_api/SpringBootJwtAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dfanso.spring_boot_jwt_auth_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
