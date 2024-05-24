c21c7395d10af3bc156e58a3b197ff7abae274e0
Leo Gavin <leogavin123@outlook.com>
2024-05-24 22:15:35 +0530

    feat: db init

--- src/main/java/com/dfanso/spring_boot_jwt_auth_api/SpringBootJwtAuthApiApplication.java
+++ src/main/java/com/dfanso/spring_boot_jwt_auth_api/SpringBootJwtAuthApiApplication.java
@@ -1,5 +1,6 @@
 package com.dfanso.spring_boot_jwt_auth_api;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootJwtAuthApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 		SpringApplication.run(SpringBootJwtAuthApiApplication.class, args);
 	}
 
